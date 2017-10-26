2e04ca613ee016431b8515bd8b0db8a270d7269b
David Billing <david.j.billing@gmail.com>
2017-10-26 16:55:44 +0200

    checked against wrong date...

--- addon/components/time-input.js
+++ addon/components/time-input.js
@@ -11,7 +11,7 @@ export default Component.extend({
     changeTime(time){
       const parsed = this.get('moment').moment(time, 'HH:mm');
       const oldDate = this.get('moment').moment(this.get('currentDate'));
-            
+
       var newDate = oldDate ? oldDate.clone() : this.get('moment').moment();
       newDate.hours(parsed.hours());
       newDate.minutes(parsed.minutes());
@@ -19,15 +19,16 @@ export default Component.extend({
       let moments = this.get('arrayDates');
       if(moments){
         // Update time in array if set
-        let idx = moments.findIndex((m) => this.get('moment').moment(m._d).diff(oldDate._d));        
+        let idx = moments.findIndex((m) => m.diff(newDate, 'minutes'));
         if(idx >= 0){
           moments[idx] = newDate;
         }
+
         this.set('arrayDates', null);
         this.set('arrayDates', moments);
       }
 
       this.sendAction('action', newDate);
     }
-  },  
+  },
 });
