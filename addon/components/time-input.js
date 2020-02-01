4625cf429ac1336491b5e9f614031d2d0bec3751
David Billing <david.j.billing@gmail.com>
2020-02-01 21:44:09 +0100

    Update to octane

--- addon/components/time-input.js
+++ addon/components/time-input.js
@@ -1,39 +1,37 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import layout from '../templates/components/time-input';
-import { get } from '@ember/object';
-import { set } from '@ember/object';
+import { action } from '@ember/object';
 
-export default Component.extend({
-  layout,
-  moment: service(),
-  tagName: 'span',
+export default class TimeInputComponent extends Component {
+  @layout
+  @service moment
+  tagName = 'span'
 
-  actions: {
-    changeTime(time) {
-      const parsed = get(this, 'moment').moment(time, 'HH:mm');
-      const oldDate = get(this, 'moment').moment(get(this, 'currentDate'));
+  @action
+  changeTime(time) {
+    const parsed = this.moment.moment(time, 'HH:mm');
+    const oldDate = this.moment.moment(this.currentDate);
 
-      let newDate = oldDate ? oldDate.clone() : get(this, 'moment').moment();
-      newDate.hours(parsed.hours());
-      newDate.minutes(parsed.minutes());
+    let newDate = oldDate ? oldDate.clone() : this.moment.moment();
+    newDate.hours(parsed.hours());
+    newDate.minutes(parsed.minutes());
 
-      let moments = get(this, 'arrayDates');
-      if (moments) {
-        // Update time in array if set
-        moments.forEach(function (element, index) {
-          if (element.diff(newDate, 'days') === 0) {
-            moments[index] = newDate;
-          }
-        }, this);
+    let moments = this.arrayDates;
+    if (moments) {
+      // Update time in array if set
+      moments.forEach(function (element, index) {
+        if (element.diff(newDate, 'days') === 0) {
+          moments[index] = newDate;
+        }
+      }, this);
 
-        set(this, 'arrayDates', null);
-        set(this, 'arrayDates', moments);
-      }
-
-      if (get(this, 'onChangedTime')) {
-        get(this, 'onChangedTime')(newDate);
-      }
+      this.arrayDates = null;
+      this.arrayDates = moments;
     }
-  },
-});
+
+    if (this.onChangedTime) {
+      this.onChangedTime(newDate);
+    }
+  }
+}
