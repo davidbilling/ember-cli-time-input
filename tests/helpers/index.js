d3333e463aa933a0a7c86bfc2f3ecfe8bdabd148
David Billing <david.j.billing@gmail.com>
2024-01-03 10:45:25 +0100

    fix embroider test

--- tests/helpers/index.js
+++ tests/helpers/index.js
@@ -1,15 +1,15 @@
 import {
-  setupApplicationTest,
-  setupRenderingTest,
-  setupTest,
+  setupApplicationTest as upstreamSetupApplicationTest,
+  setupRenderingTest as upstreamSetupRenderingTest,
+  setupTest as upstreamSetupTest,
 } from 'ember-qunit';
 
 // This file exists to provide wrappers around ember-qunit's
 // test setup functions. This way, you can easily extend the setup that is
 // needed per test type.
 
-function mySetupApplicationTest(hooks, options) {
-  setupApplicationTest(hooks, options);
+function setupApplicationTest(hooks, options) {
+  upstreamSetupApplicationTest(hooks, options);
 
   // Additional setup for application tests can be done here.
   //
@@ -27,16 +27,16 @@ function mySetupApplicationTest(hooks, options) {
   // setupMirage(hooks); // ember-cli-mirage
 }
 
-function mySetupRenderingTest(hooks, options) {
-  setupRenderingTest(hooks, options);
+function setupRenderingTest(hooks, options) {
+  upstreamSetupRenderingTest(hooks, options);
 
   // Additional setup for rendering tests can be done here.
 }
 
-function mySetupTest(hooks, options) {
-  setupTest(hooks, options);
+function setupTest(hooks, options) {
+  upstreamSetupTest(hooks, options);
 
   // Additional setup for unit tests can be done here.
 }
 
-export { mySetupApplicationTest, mySetupRenderingTest, mySetupTest };
+export { setupApplicationTest, setupRenderingTest, setupTest };
